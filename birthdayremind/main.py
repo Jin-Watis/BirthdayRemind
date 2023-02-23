826c6c0ddfe6cce2d8a73e554479552f2e7839df
Jin Watis <c1193302518@outook.com>
2023-02-23 15:35:22 +0800

    修复主程序索引Date2Name时未进行异常处理的Bug 为主程序添加error的日志
    TODO: 也许日志可以封装成一个单独的函数或者装饰器

--- birthdayremind/main.py
+++ birthdayremind/main.py
@@ -1,11 +1,13 @@
 ﻿# coding=UTF-8
-import json
-import pytz
 import datetime
+import json
+from sys import exit
+
+import pytz
+from win10toast import ToastNotifier
+
 import birthdayremind
 import change.ChangeMain as Manager
-from sys import exit
-from win10toast import ToastNotifier
 
 
 class Start:
@@ -23,14 +25,24 @@ class Start:
         self.times = '{}-{}-{}'.format(self.year, self.month, self.day)
         self.exact_time = self.t.replace(microsecond=0, tzinfo=None)
 
-        self.run()
+        try:
+            self.run()
+        except Exception as er:
+            with open(r'datas/runtime.log', 'a+') as f:
+                txt = "{}   error   {}    {}\n".format(self.exact_time, er.__class__.__name__, er)
+                f.write(txt)
 
     def run(self):
+
         self.toaster.show_toast("BirthdayRemind", "启动成功",
                                 icon_path=None, duration=2, threaded=False)
+
         self.IfAlreadyRun()
 
-        self.name = self.Date2Name[(self.month, self.day)]
+        try:
+            self.name = self.Date2Name[(self.month, self.day)]
+        except KeyError:
+            self.name = []
         if self.name:  # list不为空
             self.toaster.show_toast("今天是{}的生日".format("/".join(self.name)), "BirthdayRemind",
                                     icon_path=None, duration=10, threaded=True)
@@ -56,5 +68,3 @@ class Start:
         with open(path, 'r', encoding='utf-8') as f:
             self.Name2Date = json.load(f)
         self.Date2Name = Manager.ChangeData2_Date2Name(self.Name2Date)
-
-
